fb2495a9b0bb196ab85c6e2d4bd4ad114afc9956
Shuangjie Tian -X (shutian - Altran-Beyondsoft at Cisco) <shutian@cisco.com>
2017-10-17 17:43:28 +0800

    Add chinese input

--- Scroll.py
+++ Scroll.py
@@ -8,6 +8,7 @@ driver.get("http://www.baidu.com")
 
 #搜索
 driver.find_element_by_id("kw").send_keys("selenium")
+driver.find_element_by_id("kw").send_keys(u"上海")    #输入中文
 driver.find_element_by_id("su").click()
 time.sleep(3)
 
